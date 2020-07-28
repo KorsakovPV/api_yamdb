ad31247ee0a7a820b1bb6f813f84df5f0b58288e
Korsakov Pavel <pavelkpv@gmail.com>
2020-07-28 18:33:18 +0800

    Creating a project skeleton. Creating app. Creating model.

---
+++ content/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Title(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
