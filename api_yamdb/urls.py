5f6f4f42d7a9abdecc3daef7cd6e96fa52a25d24
Владимир <Stasfatgrowler@yandex.ru>
2020-07-28 16:03:36 +1000

    New path

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -20,4 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
+    # добавил новый путь
 ]
