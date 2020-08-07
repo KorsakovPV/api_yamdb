28dcae44bd7b4fb6dacaf7d0c08850a854fb96e1
Korsakov Pavel <pavelkpv@gmail.com>
2020-08-08 00:21:29 +0800

    fix v1.01

--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, viewsets
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from users import permissions
 from users.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
@@ -19,29 +19,22 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['user__username', ]
 
-
-class APIUser(APIView):
     # TODO Все это относится к юзерам, а вьюсет для них уже есть. Даже в урле это внутри одного ресурса /users/
     # Поэтому лучше отказаться от отдельной вьюхи и сделать это все кастомным экшеном внутри готового вьюсета
-    def get(self, request):
-        if request.user.is_authenticated:
-            # TODO red это делается пермишеном
-            user = get_object_or_404(User, id=request.user.id)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        return Response('You are not authenticated',
-                        status=status.HTTP_401_UNAUTHORIZED)
-
-    def patch(self, request):
-        if request.user.is_authenticated:
-            # TODO red Это тоже
-            user = get_object_or_404(User, id=request.user.id)
-            serializer = UserSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                # TODO red в случаях, когда нет особой хитрой обработки ошибок сериализатора, намного проще и лучше писать не через иф, а прокинув параметр raise_exception, чтобы избежать лишней вложенности и ручной обработки ошибок
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-        return Response('You are not authenticated',
-                        status=status.HTTP_401_UNAUTHORIZED)
+    # TODO red в случаях, когда нет особой хитрой обработки ошибок сериализатора, намного проще и лучше писать не через иф, а прокинув параметр raise_exception, чтобы избежать лишней вложенности и ручной обработки ошибок
+    # TODO red Это тоже
+    @action(methods=['patch', 'get'],
+            permission_classes=[IsAuthenticated],
+            detail=False,
+            url_path='me',
+            url_name='me')
+    def me(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = self.get_serializer(user)
+        if self.request.method == 'PATCH':
+            serializer = self.get_serializer(user,
+                                             data=request.data,
+                                             partial=True)
+            serializer.is_valid()
+            serializer.save()
+        return Response(serializer.data)
