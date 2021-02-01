66a563def5a0d27dc54f22403180b054adef6750
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-02 05:07:47 +0700

    add docstring

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,14 @@
+"""Файлом конфигурации для самого приложения users."""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    Подключение приложения users.
+
+    Для настройки приложения создаем класс наследник AppConfig и указываем
+    путь для его импорта в INSTALLED_APPS.
+
+    """
+
     name = 'users'
--- users/models.py
+++ users/models.py
@@ -1,17 +1,23 @@
+"""Модели приложения USERS."""
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class Role(models.TextChoices):
+    """Пользовательские роли."""
+
     # TODO gray гет-текст нормальная тема, но сейчас вы себе просто
     #  усложнили работу немного
+    #  переделать на рранение в базе.
     USER = 'user', _('User')
     MODERATOR = 'moderator', _('Moderator')
     ADMIN = 'admin', _('Admin')
 
 
 class User(AbstractUser):
+    """Класс для хранения пользователей."""
+
     email = models.EmailField(_('email address'), unique=True)
     bio = models.TextField(max_length=300, blank=True)
     confirmation_code = models.CharField(max_length=60, blank=True)
@@ -22,8 +28,10 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
+        """Метод возвращает True если пользователь админ."""
         return self.role == Role.ADMIN or self.is_staff
 
     @property
     def is_moderator(self):
+        """Метод возвращает True если пользователь модератор."""
         return self.role == Role.MODERATOR
